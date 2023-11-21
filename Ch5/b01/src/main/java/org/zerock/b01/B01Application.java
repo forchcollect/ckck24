5ee8a3f67d82e2b8e81d8d4459a03732ec177bcf
forchcollect <forchcollect@naver.com>
2023-11-21 20:08:52 +0900

    5.3 Spring Data JPA
    JPA : 엔티티 객체 @Entity //고유식별 - @Id
    
    Spring Data JPA - ex) Querydsl //인더페이스 : JpaRepository
    
    키 생성 전략(key generation strategy) - IDENTITY, SEQUENCE, TABLE, AUTO
    
    공통 속성 처리 : @MappedSuperClass
    - Spring Data JPA : AuditingEntityListener
      //SpringApplication.run에 @EnableJpaAuditing 추가

--- Ch5/b01/src/main/java/org/zerock/b01/B01Application.java
+++ Ch5/b01/src/main/java/org/zerock/b01/B01Application.java
@@ -2,11 +2,14 @@ package org.zerock.b01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class B01Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(B01Application.class, args);
 	}
 
